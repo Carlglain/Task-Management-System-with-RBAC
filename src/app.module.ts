2d5442cbcc0aed020e0b60afb8a538cb7d1241d1
Carlstorm <carlglainnyuysemo@gmail.com>
2026-02-07 05:12:00 +0100

    feat: added environment vairables for railway deployment standards

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,38 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { User } from './typeorm/entities/User';
 import { Task } from './typeorm/entities/Task';
+
 import { UserModule } from './users/users.module';
 import { TaskModule } from './tasks/task.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'NewPassword123!',
-      database: 'usamanag',
-      entities: [User, Task],
-      synchronize: true,
-    }),
-    UserModule,
-    TaskModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '3306'),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [User, Task],
+      // synchronize: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }),
+
+    UserModule,
+    TaskModule,
   ],
   controllers: [AppController],
   providers: [AppService],
